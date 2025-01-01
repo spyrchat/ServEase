6076dece41e8c66bd9ff56555715c86b57022cc7
spyrchat <spyrchat@ece.auth.gr>
2025-01-01 18:05:45 +0200

    [Improved] comment density in Appointments.js, Clients.js, Ratings.js, Service.js and writer.js

--- controllers/Services.js
+++ controllers/Services.js
@@ -1,54 +1,76 @@
 'use strict';
 
+// Import utility module for writing JSON responses
 var utils = require('../utils/writer.js');
+// Import Services service module for handling service-related business logic
 var Services = require('../service/ServicesService');
 
-module.exports.createService = function createService (req, res, next, body) {
+// Function to create a new service
+module.exports.createService = function createService(req, res, next, body) {
+  // Call the createService method in the Services service with the provided body
   Services.createService(body)
     .then(function (response) {
+      // If the operation is successful, write the response as JSON
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // If an error occurs, write the error response as JSON
       utils.writeJson(res, response);
     });
 };
 
-module.exports.deleteService = function deleteService (req, res, next, serviceId) {
+// Function to delete an existing service
+module.exports.deleteService = function deleteService(req, res, next, serviceId) {
+  // Call the deleteService method in the Services service with the provided serviceId
   Services.deleteService(serviceId)
     .then(function (response) {
+      // If the operation is successful, write the response as JSON
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // If an error occurs, write the error response as JSON
       utils.writeJson(res, response);
     });
 };
 
-module.exports.editService = function editService (req, res, next, body, serviceId) {
+// Function to edit an existing service
+module.exports.editService = function editService(req, res, next, body, serviceId) {
+  // Call the editService method in the Services service with the provided body and serviceId
   Services.editService(body, serviceId)
     .then(function (response) {
+      // If the operation is successful, write the response as JSON
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // If an error occurs, write the error response as JSON
       utils.writeJson(res, response);
     });
 };
 
-module.exports.getService = function getService (req, res, next, serviceId) {
+// Function to retrieve details of a specific service
+module.exports.getService = function getService(req, res, next, serviceId) {
+  // Call the getService method in the Services service with the provided serviceId
   Services.getService(serviceId)
     .then(function (response) {
+      // If the operation is successful, write the response as JSON
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // If an error occurs, write the error response as JSON
       utils.writeJson(res, response);
     });
 };
 
-module.exports.searchServices = function searchServices (req, res, next, search, typeFilter, locationFilter, ratingFilter) {
+// Function to search for services based on various filters
+module.exports.searchServices = function searchServices(req, res, next, search, typeFilter, locationFilter, ratingFilter) {
+  // Call the searchServices method in the Services service with the provided search and filters
   Services.searchServices(search, typeFilter, locationFilter, ratingFilter)
     .then(function (response) {
+      // If the operation is successful, write the response as JSON
       utils.writeJson(res, response);
     })
     .catch(function (response) {
+      // If an error occurs, write the error response as JSON
       utils.writeJson(res, response);
     });
 };
--- utils/writer.js
+++ utils/writer.js
@@ -1,43 +1,61 @@
+// Constructor function to create a ResponsePayload object
+// Represents an HTTP response payload with a code and payload data
 var ResponsePayload = function(code, payload) {
-  this.code = code;
-  this.payload = payload;
+  this.code = code;        // HTTP status code
+  this.payload = payload;  // Response data
 }
 
+// Function to create a new ResponsePayload object
+// @param {number} code - HTTP status code
+// @param {any} payload - Response payload (can be an object, string, etc.)
+// @returns {ResponsePayload} - A new ResponsePayload instance
 exports.respondWithCode = function(code, payload) {
   return new ResponsePayload(code, payload);
 }
 
+// Function to write a JSON response to the HTTP response object
+// @param {Object} response - HTTP response object
+// @param {any} arg1 - Either the response payload or a ResponsePayload instance
+// @param {number} [arg2] - Optional HTTP status code
 var writeJson = exports.writeJson = function(response, arg1, arg2) {
-  var code;
-  var payload;
+  var code;    // Variable to hold the HTTP status code
+  var payload; // Variable to hold the response payload
 
+  // Check if arg1 is a ResponsePayload instance
   if(arg1 && arg1 instanceof ResponsePayload) {
+    // If arg1 is a ResponsePayload, extract its payload and code
     writeJson(response, arg1.payload, arg1.code);
     return;
   }
 
+  // Determine the HTTP status code (code) from arg2 or arg1
   if(arg2 && Number.isInteger(arg2)) {
     code = arg2;
-  }
-  else {
+  } else {
     if(arg1 && Number.isInteger(arg1)) {
       code = arg1;
     }
   }
+
+  // Assign the payload based on the provided arguments
   if(code && arg1) {
     payload = arg1;
-  }
-  else if(arg1) {
+  } else if(arg1) {
     payload = arg1;
   }
 
+  // If no HTTP status code is provided, default to 200 (OK)
   if(!code) {
-    // if no response code given, we default to 200
     code = 200;
   }
+
+  // If the payload is an object, convert it to a pretty-printed JSON string
   if(typeof payload === 'object') {
     payload = JSON.stringify(payload, null, 2);
   }
+
+  // Set the HTTP response header to indicate JSON content
   response.writeHead(code, {'Content-Type': 'application/json'});
+  // Write the response payload to the HTTP response and end the connection
   response.end(payload);
 }
