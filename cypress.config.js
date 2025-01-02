fc55da863bcccd2022205c1211425bfc75bf1975
angekotro <kotrotsiouageliki@gmail.com>
2025-01-02 15:37:14 +0200

    [MODIFIED]: Removed unused variables from cypress.config.js again

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(_, __) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
   },
